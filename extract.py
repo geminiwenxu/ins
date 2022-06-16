0905f3d8a9866964417b94ab417fb3559b3b0f10
Wenxu <geminiwenxu@gmail.com>
2022-06-16 14:23:02 +0200

    modified extract

--- extract.py
+++ extract.py
@@ -1,11 +1,42 @@
+from datetime import datetime
+from itertools import dropwhile, takewhile
+
 import instaloader
 from instaloader import Hashtag
+
+
+def extract_hastag_date():
+    # Use parameters to save diffrent metadata
+    L = instaloader.Instaloader(download_pictures=True, download_videos=False, download_comments=False,
+                                save_metadata=True)
+
+    # Login
+    username = input("Enter your username: ")
+    L.interactive_login(username=username)
+
+    # User Query
+    search = input("Enter Hashtag: ")
+    limit = int(input("How many posts to download: "))
+
+    # Hashtag object
+    hashtags = instaloader.Hashtag.from_name(L.context, search).get_posts()
+
+    # Download Period
+    SINCE = datetime(2021, 5, 1)
+    UNTIL = datetime(2021, 3, 1)
+
+    no_of_downloads = 0
+    for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, hashtags)):
+        if no_of_downloads == limit:
+            break
+        print(post.date)
+        L.download_post(post, "#" + search)
+        no_of_downloads += 1
 
 
 def extract_hashtag():
     HASHTAG = input('Enter the Hashtag: ')
     bot = instaloader.Instaloader()
-    print(bot.context)
     hashtag = Hashtag.from_name(bot.context, HASHTAG)
     for post in hashtag.get_posts():
         bot.download_post(post, target="#" + hashtag.name)
@@ -16,13 +47,6 @@ def extract_user():
     bot = instaloader.Instaloader()
     bot.download_profile(Username, profile_pic_only=True)
     profile = instaloader.Profile.from_username(bot.context, Username)
-    print("Username: ", profile.username)
-    print("User ID: ", profile.userid)
-    print("Number of Posts: ", profile.mediacount)
-    print("Followers: ", profile.followers)
-    print("Followees: ", profile.followees)
-    print("Bio: ", profile.biography, profile.external_url)
-
     posts = profile.get_posts()
     try:
         for index, post in enumerate(posts, 1):
@@ -33,22 +57,16 @@ def extract_user():
 
 def extract_user_id():
     USERID = input('Enter the Account User ID: ')
-    L = instaloader.Instaloader()
-    # USER = "s1070771"
-    # PASSWORD = "s1070771pw"
-    # L.login(USER, PASSWORD)  # (login)
-    # L.interactive_login(USER)  # (ask password on terminal)
-    # L.load_session_from_file(USER)  # (load session created w/
-
-    Username = instaloader.Profile.from_id(L.context, USERID).username
-    profile = instaloader.Profile.from_username(L.context, Username)
+    bot = instaloader.Instaloader()
+    Username = instaloader.Profile.from_id(bot.context, USERID).username
+    profile = instaloader.Profile.from_username(bot.context, Username)
     posts = profile.get_posts()
     try:
         for index, post in enumerate(posts, 1):
-            L.download_post(post, target=f"{profile.username}_{index}")
+            bot.download_post(post, target=f"{profile.username}_{index}")
     except:
         exit()
 
 
 if __name__ == "__main__":
-    extract_hashtag()
+    extract_user_id()
